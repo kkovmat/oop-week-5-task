16a8f3b9c3fbcc6564f9c659d38b31bbc98ab052
kkovmat <kovalishinmatviy@gmail.com>
2026-03-05 18:50:28 +0200

    Added UniversityMember class

---
+++ Practice_5/UniversityMember.cs
@@ -0,0 +1,34 @@
+namespace Practice_5;
+class UniversityMember
+{
+    private string name;
+    public readonly string MemberId;
+    public string Name
+    {
+        get => name;
+        private set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new Exception("Name cannot be empty.");
+            }
+            name = value;            
+        }
+    }
+    protected List<string> actionLog;
+
+    public UniversityMember(string name, string memberId)
+    {
+        Name = name;
+        MemberId = memberId;
+    }
+    
+    virtual public void PerfomDuties()
+    {
+        if (actionLog.Count >= 5)
+        {
+            throw new Exception("Readched a daily limit of 5 duties.");
+        }
+    }
+
+}
